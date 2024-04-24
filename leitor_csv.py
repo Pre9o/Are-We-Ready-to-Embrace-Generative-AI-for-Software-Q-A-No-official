5cbb424fe7f9250861bca320c51ecfe76c95c780
Rafael Carneiro Pregardier <rafaelpregardier@gmail.com>
2024-04-24 15:19:31 -0300

    todas questoes

--- leitor_csv.py
+++ leitor_csv.py
@@ -14,12 +14,19 @@ csv_file_python = os.path.join(current_dir, 'QueryResults_python_442.csv')
 
 df = pd.read_csv(csv_file_python)
 
+            
 for index, row in df.iterrows():
-    if 'Title' in row and pd.notnull(row['Title']) and 'python' in row['Title'].lower():
+    if 'Title' in row and pd.notnull(row['Title']):
         row['Body'] = remove_html_tags(row['Body'])
         row['AcceptedAnswer'] = remove_html_tags(row['AcceptedAnswer'])
-        with open('python_questions.json', 'a') as f:
+        with open('python_questions_all.json', 'a') as f:
             f.write(json.dumps(row.to_dict(), indent=4) + '\n')
+            
+        if 'python' in row['Title'].lower():
+            row['Body'] = remove_html_tags(row['Body'])
+            row['AcceptedAnswer'] = remove_html_tags(row['AcceptedAnswer'])
+            with open('python_questions.json', 'a') as f:
+                f.write(json.dumps(row.to_dict(), indent=4) + '\n')
             
             
 csv_file_java = os.path.join(current_dir, 'QueryResults_java_182.csv')
@@ -27,8 +34,16 @@ csv_file_java = os.path.join(current_dir, 'QueryResults_java_182.csv')
 df = pd.read_csv(csv_file_java)
 
 for index, row in df.iterrows():
-    if 'Title' in row and pd.notnull(row['Title']) and 'java' in row['Title'].lower():
+    if 'Title' in row and pd.notnull(row['Title']):
         row['Body'] = remove_html_tags(row['Body'])
         row['AcceptedAnswer'] = remove_html_tags(row['AcceptedAnswer'])
-        with open('java_questions.json', 'a') as f:
+        with open('java_questions_all.json', 'a') as f:
             f.write(json.dumps(row.to_dict(), indent=4) + '\n')
+            
+        if 'java' in row['Title'].lower():
+            row['Body'] = remove_html_tags(row['Body'])
+            row['AcceptedAnswer'] = remove_html_tags(row['AcceptedAnswer'])
+            with open('java_questions.json', 'a') as f:
+                f.write(json.dumps(row.to_dict(), indent=4) + '\n')
+                
+                
