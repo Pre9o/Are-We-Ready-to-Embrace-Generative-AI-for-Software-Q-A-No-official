6d8f6f7cffaea79e1b966a64bfd088bf6062fc2b
Rafael Carneiro Pregardier <rafaelpregardier@gmail.com>
2024-04-24 14:48:26 -0300

    perguntas indentadas

--- leitor_csv.py
+++ leitor_csv.py
@@ -1,14 +1,23 @@
 import os
 import pandas as pd
+import json
 
 current_dir = os.path.dirname(__file__)
-csv_file = os.path.join(current_dir, 'QueryResults_python_442.csv')
+csv_file_python = os.path.join(current_dir, 'QueryResults_python_442.csv')
 
-df = pd.read_csv(csv_file)
-print(df.head())
+df = pd.read_csv(csv_file_python)
 
-# contar quantas linhas tem o csv
-print(df.shape[0])
+for index, row in df.iterrows():
+    if 'Title' in row and pd.notnull(row['Title']) and 'python' in row['Title'].lower():
+        with open('python_questions.json', 'a') as f:
+            f.write(json.dumps(row.to_dict(), indent=4) + '\n')
+            
+            
+csv_file_java = os.path.join(current_dir, 'QueryResults_java_182.csv')
 
-# printar as ultimas linhas
-print(df.tail())
+df = pd.read_csv(csv_file_java)
+
+for index, row in df.iterrows():
+    if 'Title' in row and pd.notnull(row['Title']) and 'java' in row['Title'].lower():
+        with open('java_questions.json', 'a') as f:
+            f.write(json.dumps(row.to_dict(), indent=4) + '\n')
